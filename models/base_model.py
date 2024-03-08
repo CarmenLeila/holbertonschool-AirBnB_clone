d43577e5bc6f71a90d2ca1b7855fb54c21b4c40e
CarmenLeila <146730823+CarmenLeila@users.noreply.github.com>
2024-03-08 23:05:46 +0100

    change some bugs

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
-        else:
+        if not hasattr(self,'id'):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
